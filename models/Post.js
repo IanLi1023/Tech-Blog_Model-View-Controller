8c85472416304fdffdf5c783f26b3380645b6de2
Ian Li <ian.li1023@gmail.com>
2022-02-03 15:08:28 -0700

    updated correct route for post/dashboard/home, created post/comment models an updated user

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const sequelize = require('../config/index');
 
 // create our Post model
 class Post extends Model {}
