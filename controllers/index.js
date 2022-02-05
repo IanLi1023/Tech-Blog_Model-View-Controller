21cda2dcd874581bae2317f0c4412ff3f39730d8
Ian Li <ian.li1023@gmail.com>
2022-02-05 15:26:46 -0700

    updated routes folder and files

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api/');
+// const Routes = require('./routes');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
-router.use('/api', apiRoutes);
+// router.use('/api', apiRoutes);
 
 module.exports = router;
