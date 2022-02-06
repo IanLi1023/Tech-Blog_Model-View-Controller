2f7179740e2e9fc49dec83704f2d911b35b7d174
Ian Li <ian.li1023@gmail.com>
2022-02-05 23:38:31 -0700

    updated css, cleaned up routes, fixed  handlebars

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -29,6 +29,7 @@ router.get('/', (req, res) => {
     })
       .then(dbPostData => {
         const posts = dbPostData.map(post => post.get({ plain: true }));
+        console.log(posts);
         res.render('homepage', {
             posts,
             loggedIn: req.session.loggedIn
@@ -92,7 +93,7 @@ router.get('/login', (req, res) => {
   
         // serialize the data
         const post = dbPostData.get({ plain: true });
-  
+        console.log(post.comments)
         // pass data to template
         res.render('single-post', {
             post,
